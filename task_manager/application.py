f1bf6cf5a6c4e805622a0b5e25352a922293ac3f
Richard Rollins <689-rrollins@users.noreply.gitlab.euclid-sgs.uk>
2024-11-07 08:39:04 +0000

    SQLAlchemy database

--- task_manager/application.py
+++ task_manager/application.py
@@ -2,8 +2,14 @@ import tomllib
 
 from flask import Flask
 
+from database import database
+
 application = Flask(__name__)
 application.config.from_file("config.toml", load=tomllib.load, text=False)
+
+database.init_app(application)
+with application.app_context():
+    database.create_all()
 
 if __name__ == '__main__':
     application.run(debug=True)
